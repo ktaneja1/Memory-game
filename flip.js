2ca3704457d573b427dedd1c999ebba2d01133c4
ktaneja1 <kanikataneja@gmail.com>
2016-03-16 14:21:14 -0400

    Create flip.js

---
+++ flip.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+  var AceOfSpades= newImage();
+  img src="http://cs.nyu.edu/courses/spring15/CSCI-UA.0061-001/newcards/images/Ace%20of%20Spades.jpg"
+  
+  $('.card').click(function() {
+  if($(this).data('flipped') == true) {
+  
+        $(this).attr('src','https://filletfamilyblog.files.wordpress.com/2013/02/d0490860-0-large.jpg');
+        $(this).data('flipped',false);
+    }
+    else {
+    $(this).attr('src','http://cs.nyu.edu/courses/spring15/CSCI-UA.0061-001/newcards/images/Ace%20of%20Spades.jpg')
+  $(this).data('flipped',true);
+  }
+  
+  });
+});
