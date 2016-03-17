dbe8cd17be43de2be30295722d00fbb2ae0aa059
Jeff Manning <jeff@pyrodesign.ca>
2016-03-17 12:32:52 -0400

    I moveod the contents of readme.MD to the newly created index.html IN flip.js there were a few small typos that i corrected.  In index.html I closed the .field DIV removed the extra HMTL element and moved the title for readme to the README.md file and I also added the jquery script which is required for the program to work.  In style.css I closed the selector with a } as this is required.

--- flip.js
+++ flip.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
 
-  var AceOfSpades= newImage();
-  img src="http://cs.nyu.edu/courses/spring15/CSCI-UA.0061-001/newcards/images/Ace%20of%20Spades.jpg"
-  
+  var AceOfSpades= new Image();
+  AceOfSpades.src="http://cs.nyu.edu/courses/spring15/CSCI-UA.0061-001/newcards/images/Ace%20of%20Spades.jpg";
+
   $('.card').click(function() {
   if($(this).data('flipped') == true) {
-  
+
         $(this).attr('src','https://filletfamilyblog.files.wordpress.com/2013/02/d0490860-0-large.jpg');
         $(this).data('flipped',false);
     }
@@ -13,6 +13,6 @@ $(document).ready(function() {
     $(this).attr('src','http://cs.nyu.edu/courses/spring15/CSCI-UA.0061-001/newcards/images/Ace%20of%20Spades.jpg')
   $(this).data('flipped',true);
   }
-  
+
   });
 });
